80814f429d0e1e8f0a71f80b65eadad560be23a8
Emil Kinzl <kinzl.emil@eclipso.at>
2023-01-13 12:31:05 +0100

    first commit

---
+++ Pages/mainwindow.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Questionnaire_Frontend.Pages
+{
+    public class mainwindowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnGetKinzl()
+        {
+
+        }
+    }
+}
