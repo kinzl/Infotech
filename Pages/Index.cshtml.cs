b0d6f2c918376194b7d7f4ae8a2931af46bdb26f
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2023-02-14 11:09:40 +0100

    LoginWindow and Button works

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,52 +7,18 @@ namespace Questionnaire_Frontend.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-    public List<int> list = new(){ 1, 2, 3, 4, 5 };
-
-    public IndexModel(ILogger<IndexModel> logger)
+    
+    public IndexModel()
     {
-        _logger = logger;
+        
     }
 
     public void OnGet()
     {
     }
 
-    public void OnGetRedirectShowExistingData()
+    public void OnGetRedirectMainWindow()
     {
-        Response.Redirect("AnswerQuestions");
-        
+        Response.Redirect("MainWindow");
     }
-    
-    // protected void Page_Load(object sender, EventArgs e)
-    // {
-    //
-    // }
-    // protected void BTNSubmit_Click(object sender, EventArgs e)
-    // {
-    //     FindControl(RDBTNTBL);
-    // }
-    // public void FindControl(Control controls)
-    // {
-    //     var radioButton = new RadioButton();
-    //     foreach(Control c in controls.Controls)
-    //     {
-    //         if (c.HasControls())
-    //         {
-    //             FindControl(c);
-    //         }
-    //         else if (c.GetType().ToString().Equals("System.Web.UI.WebControls.RadioButton"))
-    //         {
-    //             if ((c as RadioButton).Checked)
-    //             {
-    //                 Response.Wri
-    //                 Response.Write();
-    //                 Response.Write(c.ID);
-    //                 return;
-    //             }
-    //         }
-    //
-    //     }
-    // }
 }
