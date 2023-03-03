f2e2653ad82578eef894e47a1d50cfb62d852007
Emil Kinzl <kinzl.emil@eclipso.at>
2023-03-03 10:53:49 +0100

    nuget packages installed

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Reflection.Emit;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,10 +8,11 @@ namespace Questionnaire_Frontend.Pages;
 
 public class IndexModel : PageModel
 {
-    
+    public string Username = "Admin";
+    public string Password = "admin";
+
     public IndexModel()
     {
-        
     }
 
     public void OnGet()
@@ -21,4 +23,17 @@ public class IndexModel : PageModel
     {
         Response.Redirect("MainWindow");
     }
+
+    public IActionResult OnGetGetAllStudents()
+    {
+        Console.Write("Among");
+        return new OkObjectResult(Username);
+    }
+    
+    public IActionResult OnPostGetMyPostRequest()
+    {
+        Console.Write("Among: " + "id");
+        return new OkObjectResult(Username);
+    }
+    
 }
