a710b23e9d36e14a1a26300c1d21be010a3a5306
kinzl <kinzl.emil@eclipso.at>
2023-06-14 22:43:44 +0200

    PDF Report finished

---
+++ Frontend/DownloadProjectReport/CheckboxCellEvent.cs
@@ -0,0 +1,42 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace Questionnaire_Frontend.DownloadProjectReport
+{
+    public class CheckboxCellEvent : IPdfPCellEvent
+    {
+        private bool isChecked;
+
+        public CheckboxCellEvent(bool isChecked)
+        {
+            this.isChecked = isChecked;
+        }
+
+        public void CellLayout(
+            PdfPCell cell, Rectangle position, PdfContentByte[] canvases)
+        {
+            // Positioniere die Checkbox in der Zelle
+            float boxSize = 8;
+            float x = position.Left;
+            //float x = position.Left + 2;
+            float y = position.Bottom + (position.Height - boxSize) - 5;
+            //float y = position.Bottom + (position.Height - boxSize) / 2;
+
+            // Zeichne das ausgefüllte Kästchen (Checkbox) nur für ausgewählte Boxen
+            if (isChecked)
+            {
+                PdfContentByte canvas = canvases[PdfPTable.TEXTCANVAS];
+                canvas.SetColorFill(BaseColor.BLACK);
+                canvas.Rectangle(x, y, boxSize, boxSize);
+                canvas.Fill();
+            }
+
+            // Zeichne den Rand der Checkbox
+            PdfContentByte borderCanvas = canvases[PdfPTable.LINECANVAS];
+            borderCanvas.SetColorStroke(BaseColor.BLACK);
+            borderCanvas.SetLineWidth(1f);
+            borderCanvas.Rectangle(x, y, boxSize, boxSize);
+            borderCanvas.Stroke();
+        }
+    }
+}
---
+++ Frontend/Dto/PdfReportDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Questionnaire_Frontend.Dto
+{
+    public class PdfReportDto
+    {
+        public string? CompanyName { get; set; }
+        public string? TypeOfExecution { get; set; }
+        public string? Participants { get; set; }
+        public string? Scope { get; set; }
+        public string? Classification { get; set; }
+        public string? DocumentDistributor { get; set; }
+        public string? SecurityCheckType { get; set; }
+        public string? ManagementSummary { get; set; }
+
+    }
+}
